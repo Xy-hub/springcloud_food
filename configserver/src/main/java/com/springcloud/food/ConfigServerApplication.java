a78ec857aa339ff7010fa04da6e4bb874bf47fe3
Xy-hub <2447315136@qq.com>
2019-12-03 14:16:52 +0800

    微服务搭建

---
+++ configserver/src/main/java/com/springcloud/food/ConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.springcloud.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+//开启配置服务端
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class,args);
+    }
+}
---
+++ eruekaserver/src/main/java/com/springcloud/food/EurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.springcloud.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+//开启Eureka注册中心
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class,args);
+    }
+}
